5a040642710837275958ca030324e607bcd1107d
LoveField <hho1993@gmail.com>
2016-08-09 15:41:54 +0900

    Update common.js

--- portfolio/common/js/common.js
+++ portfolio/common/js/common.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-	
+	$('.btn-menu').click(function(){
+		$('.gnb .list').toggleClass('act');
+	});
+
+	$('.btn-about').click(function(){
+		$('.info').addClass('act');
+	});
+
+	$('.info .close').click(function(){
+		$('.info').removeClass('act');
+	});
 });
