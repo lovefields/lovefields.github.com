93bed597b78cce90c7c17d4be506fb9831354e87
LoveField <hho1993@gmail.com>
2016-08-11 16:56:58 +0900

    Update common.js

--- portfolio/common/js/common.js
+++ portfolio/common/js/common.js
@@ -10,4 +10,24 @@ $(document).ready(function(){
 	$('.info .close').click(function(){
 		$('.info').removeClass('act');
 	});
+
+	var item = 0;
+	$('.gnb .list li button').click(function(){
+		var val = $(this).attr('data-index');
+
+		$('.content .content-item').removeClass('end').removeClass('act');
+		if(item !== 0){
+			$('.content .content-item[data-index="'+ item +'"]').addClass('end');
+		}
+		$('.content .content-item[data-index="'+ val +'"]').addClass('act');
+
+		item = val;
+		return;
+	});
+
+	$('.content .content-item .close').click(function(){
+		item = 0;
+		$('.content .content-item.act').removeClass('act').addClass('end');
+		return;
+	});
 });
