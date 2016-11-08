da29eabd95a2ebf8354984157b1488565f237f5e
LoveField <hho1993@gmail.com>
2016-11-09 05:55:38 +0900

    protfolio

---
+++ README.md
@@ -0,0 +1,15 @@
+# lovefield's Site
+<h4>Portpolio</h4>
+<ol>
+  <li>lovefield's portfolio <a href="https://lovefields.github.io/portfolio/" target="_blank">link</a></li>
+</ol>
+
+<h4>Translation</h4>
+<ol>
+  <li>TR index : TR list <a href="https://lovefields.github.io/TR" target="_blank">link</a></li>
+</ol>
+
+<h4>blog</h4>
+<ol>
+  <li>lovefield's blog <a href="https://lovefields.github.io/blog" target="_blank">link</a></li>
+</ol>
---
+++ portfolio/common/js/common.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+	var width = $(window).width();
+
+	$('.btn-menu').click(function(){
+		$('.gnb .list').toggleClass('act');
+		$('.gnb .notice').addClass('hide');
+	});
+
+	$('.btn-about').click(function(){
+		$('.info').addClass('act');
+	});
+
+	$('.info .close').click(function(){
+		$('.info').removeClass('act');
+	});
+
+	var item = 0;
+	$('.gnb .list li button').click(function(){
+		var val = $(this).attr('data-index');
+
+		if(item !== val){
+			if(width < 768){
+				$('.gnb .list').removeClass('act');
+			}
+
+
+			$('.content .content-item').removeClass('end').removeClass('act');
+			if(item !== 0){
+				$('.content .content-item[data-index="'+ item +'"]').addClass('end');
+			}
+			$('.content .content-item[data-index="'+ val +'"]').addClass('act');
+
+			item = val;
+			return;
+		}
+	});
+
+	$('.content .content-item .close').click(function(){
+		item = 0;
+		$('.content .content-item.act').removeClass('act').addClass('end');
+		return;
+	});
+
+	$(window).resize(function(){
+		width = $(window).width();
+		return;
+	});
+});
