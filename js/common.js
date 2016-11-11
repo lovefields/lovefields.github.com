816103e684d471084ac7eeab144de8156e5ec443
LoveField <hho1993@gmail.com>
2016-11-11 15:06:56 +0900

    blog

--- js/common.js
+++ js/common.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-	
+	var width = $(window).width();
+
+	if(width > 970){
+		$('.category_menu .btn').click(function(){
+			$('.category_menu .list').toggleClass('act');
+		});
+	}
 });
